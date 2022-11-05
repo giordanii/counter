f610dd692a6a8d3be6bbc7597ded2ae08b2e2e4c
João Giordani <jgiordanii@gmail.com>
2022-11-05 00:02:25 -0300

    final commit

--- script.js
+++ script.js
@@ -4,11 +4,8 @@ var currentNumber = 0;
 function increase() {
     currentNumber = ++currentNumber;
     modify.innerHTML = currentNumber;
-    if (currentNumber > 0) {
-        modify.classList.add("positive");
-    }
-    else {
-        modify.classList.remove("positive");
+    if (currentNumber >= 0) {
+        modify.classList.remove("color")
     }
 }
 
@@ -16,9 +13,6 @@ function decrease() {
     currentNumber = --currentNumber;
     modify.innerHTML = currentNumber;
     if (currentNumber < 0) {
-        modify.classList.add("negative");
-    }
-    else {
-        modify.classList.remove("negative");
+        modify.classList.add("color")
     }
 }
